9ae89f36f9708cb85336436ada5da4b5a5a910d0
MauriBoye <mauriboye@gmail.com>
2021-06-02 08:42:52 -0300

    Primera versión ejercicio N°7

---
+++ Archivos/tp5ej7.py
@@ -0,0 +1,45 @@
+################
+# Mauricio Boyé - @MauriBoye
+# Ejercicio 7 - TP N°5
+# UNRN Andina - Introducción a la Ingenieria en Computación
+################
+"""
+Ejercicio N°7
+"""
+import tp5ej1
+
+def ingreso_numero_flotante(mensaje):
+    """
+    Esta funcion muestra un mensaje para indicar el ingreso
+    de un número flotante.
+    """
+    ingreso = input(mensaje)
+    try:
+        flotante = float(ingreso)
+    except ValueError as err:
+        raise tp5ej1.IngresoIncorrecto(f"'{ingreso}' no es un número!") from err
+    return flotante
+
+def distancia(puntoA, puntoB):
+    """
+    Esta funcion muestra la distancia entre dos puntos
+    """
+    if puntoB >= puntoA:
+        distancia = (puntoB) - (puntoA)
+    if puntoB < puntoA:
+        distancia = (puntoA) - (puntoB)
+    return distancia
+
+def prueba():
+    tp5ej1.marco("distancia()")
+    puntoA = ingreso_numero_flotante("Ingrese el punto A del recorrido: ")
+    puntoB = ingreso_numero_flotante("Ingrese el punto B del recorrido: ")
+    if puntoB >= puntoA:
+        print(f"La distancia entre {puntoA} y {puntoB} "
+              f"es {distancia(puntoA, puntoB)}")
+    if puntoB < puntoA:
+        print(f"La distancia entre {puntoB} y {puntoA} "
+              f"es {distancia(puntoA, puntoB)}")
+    
+if __name__ == "__main__":
+    prueba()
